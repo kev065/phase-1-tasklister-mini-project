35666a6c4d745e97b58b11bdb7501b3e6f366650
KELVIN MURITHI <kelvinmurithi@live.com>
2023-10-01 00:17:27 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  const form = document.getElementById("create-task-form");
+  const body = document.querySelector('body');
+  const tasks = document.querySelector('#tasks');
+  
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const task = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.textContent = " x";
+    task.textContent = `${e.target[0].value} `;
+    task.append(btn);
+    tasks.append(task);
+    
+    btn.addEventListener('click',(e)=>{
+      e.target.parentNode.remove();
+      });
+
+    });
+  });
